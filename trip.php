e4bfdea08fda4cc3319e0121e025bb1789035b13
joelpagett2026 <joelpagett@gmail.com>
2026-08-31 08:24:34 +0100

    Load restaurant and mobile modal enhancements

--- trip.php
+++ trip.php
@@ -147,6 +147,7 @@ $dbVersion = @filemtime(__DIR__ . '/db.js') ?: time();
 $uiVersion = @filemtime(__DIR__ . '/itinerary-ui.js') ?: time();
 $mapVersion = @filemtime(__DIR__ . '/map-mobile-redesign.js') ?: time();
 $mobileDragVersion = @filemtime(__DIR__ . '/mobile-drag.js') ?: time();
+$completionVersion = @filemtime(__DIR__ . '/itinerary-completion.js') ?: time();
 $page = preg_replace('~src="/auth\.js\?v=[^"]+"~', 'src="/auth.js?v=' . $authVersion . '"', $page);
 $page = preg_replace('~src="/db\.js\?v=[^"]+"~', 'src="/db.js?v=' . $dbVersion . '"', $page);
 $drawerSwipeFix = <<<'HTML'
@@ -208,6 +209,7 @@ $page = str_replace(
   . '<script src="/map-mobile-redesign.js?v=' . $mapVersion . '"></script>' . "\n"
   . $drawerSwipeFix . "\n"
   . '<script src="/mobile-drag.js?v=' . $mobileDragVersion . '"></script>' . "\n"
+  . '<script src="/itinerary-completion.js?v=' . $completionVersion . '"></script>' . "\n"
   . '<script src="/trip-delete.js?v=1"></script>' . "\n</body>",
   $page,
   $guardCount
